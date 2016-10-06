9f352c1ffe4cea01ac86a4e8546a0b16613bd6eb
Guillaume Chieb Bouares <guillaume@horsecom.io>
2016-10-06 10:31:57 +0200

    init commit

---
+++ UrDailyStep/UrDailyStep/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UrDailyStep
+//
+//  Created by guillaume chieb bouares on 05/10/2016.
+//  Copyright © 2016 com.github.shemana. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
